46260f413e9dcd435036ffe7722bf69c18f93432
Minh Khoa <khoanguyen.1881@gmail.com>
2024-08-07 21:05:40 +0700

    Theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import theme from './theme'
 
 function App() {
 
   return (
     <>
       <div>Khoahii</div>
+      <Typography variant='body2' color='text.secondary' >
+        Test Typography
+      </Typography>
       <Button variant="contained">Hello world</Button>
       <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
+      <Button variant="contained">
+        Contained
+      </Button>
       <Button variant="outlined">Outlined</Button>
     </>
   )
