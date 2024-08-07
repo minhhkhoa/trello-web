5ed11cb89fc114acecd47442078b781f506c0326
Minh Khoa <khoanguyen.1881@gmail.com>
2024-08-07 22:36:21 +0700

    done_p1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import Button from '@mui/material/Button'
+import {
+  useColorScheme
+} from '@mui/material/styles'
 
 function App() {
 
+  function ModeToggle() {
+    const { mode, setMode } = useColorScheme()
+    return (
+      <Button
+        onClick={() => {
+          setMode(mode === 'light' ? 'dark' : 'light')
+        }}
+      >
+        {mode === 'light' ? 'Turn dark' : 'Turn light'}
+      </Button>
+    )
+  }
+
   return (
     <>
+      <ModeToggle/>
+      <hr/>
       <div>Khoahii</div>
       <Button variant="contained">Hello world</Button>
       <Button variant="text">Text</Button>
