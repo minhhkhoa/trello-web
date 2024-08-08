8d5df387ac4d854e19c1807a938807e311c415cb
Minh Khoa <khoanguyen.1881@gmail.com>
2024-08-08 16:21:08 +0700

    check

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,87 @@
 import Button from '@mui/material/Button'
+import useMediaQuery from '@mui/material/useMediaQuery'
 import {
   useColorScheme
 } from '@mui/material/styles'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
 
-function App() {
 
-  function ModeToggle() {
-    const { mode, setMode } = useColorScheme()
-    return (
-      <Button
-        onClick={() => {
-          setMode(mode === 'light' ? 'dark' : 'light')
-        }}
-      >
-        {mode === 'light' ? 'Turn dark' : 'Turn light'}
-      </Button>
-    )
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+
+
+  const handleChange = (event) => {
+    setMode(event.target.value)
   }
 
   return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <div style={{ display: 'flex', alignItems: 'center', gap:'8px' }}>
+            <LightModeIcon fontSize='small' />
+            Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+          <div style={{ display: 'flex', alignItems: 'center', gap:'8px' }}>
+            <DarkModeOutlinedIcon fontSize='small' />
+            Dark
+          </div>
+
+        </MenuItem>
+        <MenuItem value="system">
+          <div style={{ display: 'flex', alignItems: 'center', gap:'8px' }}>
+            <SettingsBrightnessIcon fontSize='small' />
+            System
+          </div>
+
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+
+  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)') //lấy giá trị light:dark từ system clent
+  // const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)')
+  // console.log('prefersDarkMode: ', prefersDarkMode)
+  // console.log('prefersLightMode: ', prefersLightMode)
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
+function App() {
+  return (
     <>
-      <ModeToggle/>
-      <hr/>
+      <ModeSelect />
+      <hr />
+
+      <ModeToggle />
+      <hr />
+
       <div>Khoahii</div>
       <Button variant="contained">Hello world</Button>
       <Button variant="text">Text</Button>
